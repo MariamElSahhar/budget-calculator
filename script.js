707b367fcc5cdbee01b74ecd8fabd71fe3d6740d
MariamELSahhar <me1693@nyu.edu>
2023-03-31 11:50:52 +0400

    html, css, and javascript skeleton
    html, css, javascript skeleton done

---
+++ script.js
@@ -0,0 +1,15 @@
+function calculateBudget() {
+    var days = parseInt(document.getElementById("days").value);
+    var people = parseInt(document.getElementById("people").value);
+    var food = document.getElementById("food").checked;
+    var transport = document.getElementById("transport").checked;
+    var lodging = document.getElementById("lodging").checked;
+  
+    var budget = 0;
+    if (food) budget += 50 * days * people;
+    if (transport) budget += 25 * days * people;
+    if (lodging) budget += 100 * days * people;
+  
+    document.getElementById("result").value = "$" + budget;
+  }
+  
