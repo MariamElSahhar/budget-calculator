d44f6400aa210684d84e5942c3074b3e4048adca
MariamElSahhar <me1693@nyu.edu>
2023-07-09 13:54:04 +0400

    added prices as vars

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+// when the page is loaded
 document.addEventListener("DOMContentLoaded", function() {
   var inputs = document.querySelectorAll('input[type="number"]');
   
   inputs.forEach(function(input) {
     input.addEventListener('input', function() {
+      // ensure empty input fields are set to 0
       if(input.value == '')
         input.value = 0;
+      // remove the first 0 if a number is entered
       else if (input.value !== '0' && input.value.startsWith('0'))
           input.value = input.value.slice(1);
       calculateBudget();
     });
   });
-  const click = document.getElementById("result");
 
+  // copy the value upon clicking the result button  
+  const click = document.getElementById("result");
   click.addEventListener("click", function() {
-    // click.select();
     navigator.clipboard.writeText(click.value);
-    console.log("clicked");
     click.classList.add("clicked");
     setTimeout(function() {
       click.classList.remove('clicked');
@@ -23,8 +25,35 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-
+// calculate the budget
 function calculateBudget() {
+  // set price
+  const full_day_catering_price = 265;
+  const half_day_catering_price = 200;
+  const photography_only_price = 2700;
+  const videography_price = 3225;
+  const full_event_recording_price = 3000;
+  const stage_backdrop_price = 9750;
+  const backdrop_banner_price = 3750;
+  const roll_up_banner_price = 275;
+  const branded_cube_design_32cm_price = 500;
+  const branded_cube_design_50cm_price = 1000;
+  const photo_frame_prop_price = 350;
+  const trophies_price = 225;
+  const floor_stickers_price = 43;
+  const water_bottle_tags_price = 8;
+  const certificate_printing_price = 18;
+  const gift_box_price = 400;
+  const totebag_gift_set_price = 35;
+  const power_bank_price = 85;
+  const water_bottle_price = 40;
+  const pens_price = 25;
+  const a5_notebook_price = 25;
+  const cotton_tote_bags_price = 15;
+  const laptop_stickers_price = 15;
+  const badges_lanyards_price = 15;
+
+  // retrieve values by id from the html
   var venue = parseInt(document.getElementById("venue").value);
   var attendees = parseInt(document.getElementById("attendees").value);
   var full_day_catering = parseInt(document.getElementById("full-day-catering").value);
@@ -52,10 +81,35 @@ function calculateBudget() {
   var laptop_stickers = parseInt(document.getElementById("laptop-stickers").value);
   var badges_lanyards = parseInt(document.getElementById("badges-lanyards").value);
   
-  
+  // calculate total price  
   var budget = 0;
-  budget = venue + full_day_catering * attendees * 265 + half_day_catering * attendees * 200 + photography_only * 2700 + videography * 3225 + full_event_recording * 3000 + stage_backdrop * 9750 + backdrop_banner * 3750 + roll_up_banner * 275 + branded_cube_design_32cm * 500 + branded_cube_design_50cm * 1000 + photo_frame_prop * 350 + trophies * 225 + floor_stickers * 43 + water_bottle_tags * 8 + certificate_printing * 18 + gift_box * 400 + totebag_gift_set * 35 + power_bank * 85 + water_bottle * 40 + pens * 25 + a5_notebook * 25 + cotton_tote_bags * 15 + laptop_stickers * 15 + badges_lanyards * 15;
-  
+  const budget = venue +
+  full_day_catering * attendees * full_day_catering_price +
+  half_day_catering * attendees * half_day_catering_price +
+  photography_only * photography_only_price +
+  videography * videography_price +
+  full_event_recording * full_event_recording_price +
+  stage_backdrop * stage_backdrop_price +
+  backdrop_banner * backdrop_banner_price +
+  roll_up_banner * roll_up_banner_price +
+  branded_cube_design_32cm * branded_cube_design_32cm_price +
+  branded_cube_design_50cm * branded_cube_design_50cm_price +
+  photo_frame_prop * photo_frame_prop_price +
+  trophies * trophies_price +
+  floor_stickers * floor_stickers_price +
+  water_bottle_tags * water_bottle_tags_price +
+  certificate_printing * certificate_printing_price +
+  gift_box * gift_box_price +
+  totebag_gift_set * totebag_gift_set_price +
+  power_bank * power_bank_price +
+  water_bottle * water_bottle_price +
+  pens * pens_price +
+  a5_notebook * a5_notebook_price +
+  cotton_tote_bags * cotton_tote_bags_price +
+  laptop_stickers * laptop_stickers_price +
+  badges_lanyards * badges_lanyards_price;
+
+  // print price followed by "AED"
   document.getElementById("result").value = budget.toLocaleString() + " AED";
 }
   
